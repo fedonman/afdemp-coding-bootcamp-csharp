758ec075a20b66c7514d82c1cd6232972d380db8
fedonman <fedonman@gmail.com>
2016-10-24 18:05:22 +0300

    Upload Solutions

---
+++ Week 03/Solutions/ExerciseLibrary.sql
@@ -0,0 +1,92 @@
+﻿use master;
+GO
+IF EXISTS(select * from sys.databases where name='ExerciseLibrary')
+	drop database ExerciseLibrary;
+GO
+create database ExerciseLibrary;
+GO
+use ExerciseLibrary;
+GO
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Authors')
+	drop table Authors;
+
+create table Authors (
+	ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(128) NOT NULL
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Books')
+	drop table Books;
+create table Books (
+	ISBN int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Title varchar(128) NOT NULL
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'BookAuthors')
+	drop table BookAuthors;
+create table BookAuthors (
+	BookISBN int NOT NULL FOREIGN KEY REFERENCES Books(ISBN),
+	AuthorID int NOT NULL FOREIGN KEY REFERENCES Authors(ID),
+	CONSTRAINT BookAuthorsID PRIMARY KEY (BookISBN, AuthorID)
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Libraries')
+	drop table Libraries;
+create table Libraries (
+	ID int NOT NULL IDENTITY(1,1) PRIMARY KEY
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'LibraryBooks')
+	drop table LibraryBooks;
+create table LibraryBooks (
+	LibraryID int NOT NULL FOREIGN KEY REFERENCES Libraries(ID),
+	BookISBN int NOT NULL FOREIGN KEY REFERENCES Books(ISBN),
+	PhysicalCopies int NOT NULL DEFAULT 0,
+	AvailableCopies int NOT NULL DEFAULT 0,
+	CONSTRAINT LibraryBooksID PRIMARY KEY (LibraryID, BookISBN)
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Users')
+	drop table Users;
+create table Users (
+	ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(128) NOT NULL
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'LibraryUsers')
+	drop table LibraryUsers;
+create table LibraryUsers (
+	LibraryID int NOT NULL FOREIGN KEY REFERENCES Libraries(ID),
+	UserID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
+	CONSTRAINT LibraryUsersID PRIMARY KEY (LibraryID, UserID)
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Librarians')
+	drop table Librarians;
+create table Librarians (
+	ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(128) NOT NULL,
+	ManagedLibraryID int NOT NULL FOREIGN KEY REFERENCES Libraries(ID)
+);
+
+if exists(select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Rents')
+	drop table Rents;
+create table Rents (
+	ID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	UserID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
+	BookISBN int NOT NULL FOREIGN KEY REFERENCES Books(ISBN),
+	LibraryID int NOT NULL FOREIGN KEY REFERENCES Libraries(ID),
+	AuthorizedBy int NOT NULL FOREIGN KEY REFERENCES Librarians(ID),
+	RentDate date NOT NULL DEFAULT GETDATE()
+)
+
+INSERT INTO Authors (Name) VALUES ('Tom Robbins');
+INSERT INTO Authors (Name) VALUES ('Fyodor Dostoyevsky');
+
+INSERT INTO Books (Title) VALUES ('Even Cowgirls Get the Blues');
+INSERT INTO Books (Title) VALUES ('Half Asleep in Frog Pijamas');
+INSERT INTO Books (Title) VALUES ('Villa Incognito');
+
+INSERT INTO BookAuthors VALUES(1, 1);
+INSERT INTO BookAuthors VALUES(2, 1);
+INSERT INTO BookAuthors VALUES(3, 1);
---
+++ Week 03/Solutions/ExerciseSimpleQueries.sql
@@ -0,0 +1,19 @@
+﻿/*1. Get all columns from the tables Customers, Orders and Suppliers */
+select * from customers;
+select * from orders;
+select * from suppliers;
+
+/* 2. Get all Customers alphabetically, by Country and name */
+select * from customers order by Country, ContactName;
+
+/* 3. Get all Orders by date */
+select * from orders order by OrderDate;
+
+/* 4. Get the count of all Orders made during 1997 */
+select count(*) from orders where OrderDate between '1997-01-01' and '1997-12-31 23:59:59'
+
+/* 5. Get the names of all the contact persons where the person is a manager, alphabetically */
+select contactname from Customers where ContactTitle like '%Manager%' order by ContactName;
+
+/* 6. Get all orders placed on the 19th of May, 1997  */
+select * from orders where OrderDate = '1997-05-19';
